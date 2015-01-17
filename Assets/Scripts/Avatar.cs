4075404753e5740b62febc8a77269993188409c2
Zac <brickman1444@gmail.com>
2015-01-17 16:38:00 -0600

    tearing off fps controls and putting in bad third person controls. Collision now works because it has a rigidbody.

--- Assets/Scripts/Avatar.cs
+++ Assets/Scripts/Avatar.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Avatar : MonoBehaviour {
 
     public int resourceCount;
+    public float rotSpeed;
+    public float linearSpeed;
 
 	// Use this for initialization
 	void Start () {
@@ -12,11 +14,16 @@ public class Avatar : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    
+        transform.Rotate(Vector3.up, Input.GetAxis("Horizontal") * rotSpeed);
+
+        transform.position += Input.GetAxis("Vertical") * transform.forward * linearSpeed;
+
+        Debug.DrawLine(transform.position, transform.position + transform.forward * 10);
 	}
 
     void OnTriggerEnter(Collider other)
     {
+        Debug.Log("resource get");
         resourceCount++;
         Destroy(other.gameObject);
     }
